4c1d4e10c1f6232397ec62f5804ce07c93bce1af
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-08-16 17:12:07 +0200

    style: Fix footer text to be relevant to project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+		<p>visit <a href="https://docs.berachain.com/learn/pol/gauges">docs.berachain.com</a> to learn More</p>
 	</footer>
 </div>
 
