36c8a3aa9c94c4e90ebf6b8f5dbb965f6ca2acf6
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-08-17 09:30:34 +0200

    fix: add new adapter to support gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import staticAdapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel'
 
 const dev = process.env.NODE_ENV === 'development';
+const isGitHubPages = process.env.ADAPTER === 'static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: isGitHubPages ? staticAdapter() : vercel(),
 		paths: {
 			base: dev ? '/gauge-checker' : '',
 		}
