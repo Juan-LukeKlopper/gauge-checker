bb218b7e086acf52ff3fc43c092da2fd4ff6d65e
Juan-LukeKlopper <jlklopper@protonmail.com>
2024-08-17 09:58:25 +0200

    fix: configure netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import staticAdapter from '@sveltejs/adapter-static';
-import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto';
 
 const dev = process.env.NODE_ENV === 'development';
 const isGitHubPages = process.env.ADAPTER === 'static';
@@ -10,14 +9,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: isGitHubPages ? staticAdapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}) : vercel(),
-		paths: {
-			base: dev ? '/gauge-checker' : '',
-		}
+		adapter: adapter()
 	}
 };
 
